482b9ed093dd1fedd48bdb4137ccb70f23c3159f
Carl Earvin Bedia Nonacido <carlnonacido@gmail.com>
2024-12-13 00:16:20 +0800

    update mern crash course

--- backend/server.js
+++ backend/server.js
@@ -7,12 +7,13 @@ import productRoutes from './routes/product.route.js';
 dotenv.config();
 
 const app = express();
+const PORT = process.env.PORT || 5000;
 
 app.use(express.json()); // allows us to accept JSON data in the req.body
 
 app.use("/api/products", productRoutes);
 
-app.listen(5000, () => {
+app.listen(PORT, () => {
     connectDB();
-    console.log('Server is running on port 5000');
+    console.log('Server started at https://localhost:' + PORT);
 });
---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import { Button } from '@/components/ui/provider'
+
+function App() {
+  return (
+    <>
+      <Button>Hello</Button>
+    </>
+  )
+}
+
+export default App
